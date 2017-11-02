149c6e81ccf9e16d38cf7faf0eb4c80316891d93
bertsss <robertobonifacio@creatinginfo.com>
2017-11-02 12:28:28 +0800

    updated nicknames

--- index.js
+++ index.js
@@ -7,24 +7,36 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-    socket.broadcast.emit('user connected', { id: socket.id });
-    
+io.on('connection', function(socket){    
     socket.on('new user', function(data){
         if(nicknames.indexOf(data) != -1){
-            
+            socket.emit('new user', false);
+        } else {
+            socket.emit('new user', true);
+            socket.nickname = data;
+            nicknames.push(socket.nickname);
+            updateNicknames();
+            socket.broadcast.emit('user connected', { id: socket.nickname });
+            console.log(socket.nickname + " has connected");
         }
     });
 
-    console.log(socket.id + "has connected");
-    socket.on('chat message', function(msg){
-        io.emit('chat message', msg)
+    socket.on('chat message', function(data){
+        io.emit('chat message', {msg: data, nick: socket.nickname});
     });
 
     socket.on('disconnect', function(){
-        socket.broadcast.emit('user disconnected', { id: socket.id });
-        console.log(socket.id + 'has disconnected');
+        socket.broadcast.emit('user disconnected', { id: socket.nickname });
+        console.log(socket.nickname + 'has disconnected');
+        
+        if(!socket.nickname) return;
+        nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+        updateNicknames();
     });
+
+    function updateNicknames(){
+        io.sockets.emit('usernames', nicknames);
+    }
 
 });
 
